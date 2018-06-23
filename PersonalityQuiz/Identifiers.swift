1857ab6d1cbbc328fc3699547b7ce4ff74d107e0
Sain-R Edwards <ske@SKE-iMac.local>
2018-06-22 21:37:56 -0700

    Added two more vcs to IB and built out the initial vc and results vc UI

---
+++ PersonalityQuiz/Identifiers.swift
@@ -0,0 +1,13 @@
+//
+//  Identifiers.swift
+//  PersonalityQuiz
+//
+//  Created by Sain-R Edwards on 6/22/18.
+//  Copyright © 2018 Swift Koding 4 Everyone. All rights reserved.
+//
+
+import Foundation
+
+let GO_TO_QUESTIONS_VC = "QuestionsSegue"
+
+let GO_TO_RESULTS_VC = "ResultsSegue"
