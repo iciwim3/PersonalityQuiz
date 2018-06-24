ead99b8e2dc0772d771721c9ee293f2dc5afff0e
Sain-R Edwards <ske@SKE-iMac.local>
2018-06-24 11:04:24 -0700

    Added IBOutlets for stack view buttons and labels, modified updateUI function, and created functions that would update the stack views for the current question on the view

--- PersonalityQuiz/Controller/QuestionViewController.swift
+++ PersonalityQuiz/Controller/QuestionViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class QuestionViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    
     @IBOutlet weak var singleAnswerSV: UIStackView!
+    @IBOutlet weak var singleButton1: UIButton!
+    @IBOutlet weak var singleButton2: UIButton!
+    @IBOutlet weak var singleButton3: UIButton!
+    @IBOutlet weak var singleButton4: UIButton!
+    
     @IBOutlet weak var multipleAnswerSV: UIStackView!
+    @IBOutlet weak var multipleLabel1: UILabel!
+    @IBOutlet weak var multipleLabel2: UILabel!
+    @IBOutlet weak var multipleLabel3: UILabel!
+    @IBOutlet weak var multipleLabel4: UILabel!
+    
     @IBOutlet weak var rangeAnswerSV: UIStackView!
+    @IBOutlet weak var rangedLabel1: UILabel!
+    @IBOutlet weak var rangedLabel2: UILabel!
+    
+    @IBOutlet weak var questionProgressView: UIProgressView!
     
     var questionIndex = 0
     
@@ -53,18 +69,58 @@ class QuestionViewController: UIViewController {
         multipleAnswerSV.isHidden = true
         rangeAnswerSV.isHidden = true
         
-        navigationItem.title = "Question  #\(questionIndex + 1)"
-        
         let currentQuestion = questions[questionIndex]
+        let currentAnswers = currentQuestion.answers
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        navigationItem.title = "Question  #\(questionIndex + 1)"
+        questionLabel.text = currentQuestion.text
+        questionProgressView.setProgress(totalProgress, animated: true)
         
         switch currentQuestion.type {
             case .single:
-                singleAnswerSV.isHidden = false
+                updateSingleStack(using: currentAnswers)
             case .multiple:
-                multipleAnswerSV.isHidden = false
+                updateMultipleStack(using: currentAnswers)
             case .ranged:
-                rangeAnswerSV.isHidden = false
+                updateRangedStack(using: currentAnswers)
         }
+    }
+    
+    func updateSingleStack(using answers: [Answer]) {
+        singleAnswerSV.isHidden = false
+        singleButton1.setTitle(answers[0].text, for: .normal)
+        singleButton2.setTitle(answers[1].text, for: .normal)
+        singleButton3.setTitle(answers[2].text, for: .normal)
+        singleButton4.setTitle(answers[3].text, for: .normal)
+    }
+    
+    func updateMultipleStack(using answers: [Answer]) {
+        multipleAnswerSV.isHidden = false
+        multipleLabel1.text = answers[0].text
+        multipleLabel2.text = answers[1].text
+        multipleLabel3.text = answers[2].text
+        multipleLabel4.text = answers[3].text
+    }
+    
+    func updateRangedStack(using answers: [Answer]) {
+        rangeAnswerSV.isHidden = false
+        rangedLabel1.text = answers.first?.text
+        rangedLabel2.text = answers.last?.text
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
