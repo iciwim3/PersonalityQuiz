57eae18de0e7f254ba82d6607ffe46e285cb8a19
Sain-R Edwards <ske@SKE-iMac.local>
2018-06-25 17:38:11 -0700

    Updated QuestionVC to include outlets for the different buttons in the stack views and created IBActions as well, also created functions that would help the quiz's progression

--- PersonalityQuiz/Controller/QuestionViewController.swift
+++ PersonalityQuiz/Controller/QuestionViewController.swift
@@ -23,14 +23,21 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var multipleLabel2: UILabel!
     @IBOutlet weak var multipleLabel3: UILabel!
     @IBOutlet weak var multipleLabel4: UILabel!
+    @IBOutlet weak var multiSwitch1: UISwitch!
+    @IBOutlet weak var multiSwitch2: UISwitch!
+    @IBOutlet weak var multiSwitch3: UISwitch!
+    @IBOutlet weak var multiSwitch4: UISwitch!
     
     @IBOutlet weak var rangeAnswerSV: UIStackView!
     @IBOutlet weak var rangedLabel1: UILabel!
     @IBOutlet weak var rangedLabel2: UILabel!
+    @IBOutlet weak var rangedSlider: UISlider!
     
     @IBOutlet weak var questionProgressView: UIProgressView!
     
     var questionIndex = 0
+    
+    var answersChosen: [Answer] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,6 +70,65 @@ class QuestionViewController: UIViewController {
             ])
         
     ]
+    
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+        
+        switch sender {
+            case singleButton1:
+                answersChosen.append(currentAnswers[0])
+            case singleButton2:
+                answersChosen.append(currentAnswers[1])
+            case singleButton3:
+                answersChosen.append(currentAnswers[2])
+            case singleButton4:
+                answersChosen.append(currentAnswers[3])
+            default:
+                break
+        }
+        
+        nextQuestion()
+        
+    }
+    
+    @IBAction func multipleAnswerButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+        
+        if multiSwitch1.isOn {
+            answersChosen.append(currentAnswers[0])
+        }
+        if multiSwitch2.isOn {
+            answersChosen.append(currentAnswers[1])
+        }
+        if multiSwitch3.isOn {
+            answersChosen.append(currentAnswers[2])
+        }
+        if multiSwitch4.isOn {
+            answersChosen.append(currentAnswers[3])
+        }
+        
+        nextQuestion()
+        
+    }
+    
+    @IBAction func rangedAnswerButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+        let index = Int(round(rangedSlider.value * Float(currentAnswers.count - 1)))
+        
+        answersChosen.append(currentAnswers[index])
+        
+        nextQuestion()
+    }
+    
+    func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+        } else {
+            performSegue(withIdentifier: GO_TO_RESULTS_VC, sender: nil)
+        }
+    }
     
     func updateUI() {
         singleAnswerSV.isHidden = true
@@ -97,6 +163,10 @@ class QuestionViewController: UIViewController {
     
     func updateMultipleStack(using answers: [Answer]) {
         multipleAnswerSV.isHidden = false
+        multiSwitch1.isOn = false
+        multiSwitch2.isOn = false
+        multiSwitch3.isOn = false
+        multiSwitch4.isOn = false
         multipleLabel1.text = answers[0].text
         multipleLabel2.text = answers[1].text
         multipleLabel3.text = answers[2].text
@@ -105,6 +175,7 @@ class QuestionViewController: UIViewController {
     
     func updateRangedStack(using answers: [Answer]) {
         rangeAnswerSV.isHidden = false
+        rangedSlider.setValue(0.5, animated: false)
         rangedLabel1.text = answers.first?.text
         rangedLabel2.text = answers.last?.text
     }
